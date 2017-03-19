73b8c34385556e09938ea1bf33625404afbf07df
Kamila Kosmowska <kamilawkosmowska@gmail.com>
2017-03-19 23:52:40 +0100

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,29 @@
+var n = prompt("Podaj wysokość choinki: ", "");
+
+k=1;
+for (i=0;i<n; i++)
+{
+	
+		for (j=0;j<k; j++)
+		{
+			if(i<n-1)
+			{
+				if(j==i && i!=0)
+					document.write("<span class=\"brown\">*</span>");
+				else if (i==0)
+					document.write("<span class=\"star\">*</span>");
+				else 
+					document.write("*")
+			}
+			else
+			{
+				if(j!=i)
+					document.write("<span class=\"invisible\">_</span>")
+				else
+					document.write("<span class=\"brown\">*</span>")
+			}
+		}
+	
+	document.write("<br/>")
+	k+=2;
+}
